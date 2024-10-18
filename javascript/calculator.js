ed83f66a2432e85176e6c429f7c3fb95ed95f861
shais91 <muhammadshais1991@gmail.com>
2024-10-19 01:05:45 +0500

    style basic calculator and worked on logic

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -4,71 +4,73 @@ let number1;
 let number2;
 let operator;
 let opMultiply = document.querySelector("#multiply");
+let classOpButton = document.querySelector(".opButton");
 let opDivide = document.querySelector("#divide");
 let opAdd = document.querySelector("#add");
 let opSubtract = document.querySelector("#subtract");
 let opEqual = document.querySelector("#equal");
 let numbC = document.querySelector("#AC");
-let numb1 = document.querySelector("#one");
-let numb2 = document.querySelector("#two");
-let numb3 = document.querySelector("#three");
-let numb4 = document.querySelector("#four");
-let numb5 = document.querySelector("#five");
-let numb6 = document.querySelector("#six");
-let numb7 = document.querySelector("#seven");
-let numb8 = document.querySelector("#eight");
-let numb9 = document.querySelector("#nine");
-let numb0 = document.querySelector("#zero");
 let numbdecimal = document.querySelector("#decimal");
 const CONTNUMBER = document.querySelector(".numbers");
 const CONTOPERATOR = document.querySelector(".operators");
 const CALCDISPLAY = document.querySelector("#display");
 let OPCLICK = false;
+let opDone = false;
 const DISPNUM = [];
 
-// Event listeners
+// EVENT LISTENERS
 
-numbC.addEventListener("click", (e) => {
+numbC.addEventListener("click", e => {
   clear();
   e.stopPropagation();
 });
 
-// numbers event listner
-CONTNUMBER.addEventListener("click", (event) => {
+// NUMBER BUTTON EVENT LISTENER
+
+CONTNUMBER.addEventListener("click", event => {
   const isButton = event.target.nodeName === "BUTTON";
   if (!isButton) {
     return;
   }
-  if ((OPCLICK = true)) {
-    DISPNUM.push(event.target.textContent);
-    CALCDISPLAY.textContent = DISPNUM.join(" ");
-    number2 = DISPNUM.join("");
-    console.log(number2);
-    return;
+  if (CALCDISPLAY.textContent !== "0" && opDone) {
+    DISPNUM.splice(0);
+    CALCDISPLAY.textContent = "";
   }
   DISPNUM.push(event.target.textContent);
-  CALCDISPLAY.textContent = DISPNUM.join(" ");
+  CALCDISPLAY.textContent = DISPNUM.join("");
+
+  if (OPCLICK) {
+    number2 = DISPNUM.join("");
+  } else {
+    number1 = DISPNUM.join("");
+  }
   OPCLICK = false;
+  opDone = false;
 });
 
-// operator event listener
-CONTOPERATOR.addEventListener("click", (event) => {
+// OPERATOR EVENT listener
+
+CONTOPERATOR.addEventListener("click", event => {
   const isButton = event.target.nodeName === "BUTTON";
   if (!isButton) {
     return;
+  } else if (OPCLICK === false && opDone) {
+    classOpButton.style.backgroundColor = "#596fee";
   }
-
+  let button = event.target;
+  button.style.backgroundColor = "#ec5353";
   operator = event.target.textContent;
-  number1 = DISPNUM.join("");
   OPCLICK = true;
   DISPNUM.splice(0);
 });
 
-// equal button event listener
-opEqual.addEventListener("click", (e) => {
+// EQUAL button event listener
+
+opEqual.addEventListener("click", e => {
   calculate(number1, number2);
   e.stopPropagation();
 });
+
 // Functions
 
 let add = (x, y) => x + y;
@@ -83,7 +85,7 @@ let clear = () => {
   number2 = undefined;
 };
 
-let calculate = (x, y) => {
+const calculate = (x, y) => {
   if (number1 === undefined || number2 === undefined) {
     return;
   }
@@ -93,16 +95,22 @@ let calculate = (x, y) => {
       answer = add(+x, +y);
       break;
     case "-":
-      answer = subtract(x, y);
+      answer = subtract(+x, +y);
       break;
     case "*":
-      answer = multiply(x, y);
+      answer = multiply(+x, +y);
       break;
     case "/":
-      answer = divide(x, y);
+      if (y === "0") {
+        answer = "Undefined";
+      } else answer = divide(+x, +y);
       break;
   }
+
   CALCDISPLAY.textContent = answer;
+  opDone = true;
+  OPCLICK = false;
+  classOpButton.style.backgroundColor = "#596fee";
   console.log(answer, number1, number2);
   console.log(typeof number1);
 };
